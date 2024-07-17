b610af7c52a331371a7f6c577efa015f360fe839
Ironicsus <128300959+Ironicsus@users.noreply.github.com>
2024-07-17 17:46:04 +0530

    Update client_state.rs
    Reformat blanket implementation for readability
    
    Signed-off-by: Ironicsus <128300959+Ironicsus@users.noreply.github.com>

--- ibc-core/ics02-client/context/src/client_state.rs
+++ ibc-core/ics02-client/context/src/client_state.rs
@@ -249,7 +249,8 @@ pub trait ClientState<V: ClientValidationContext, E: ClientExecutionContext>:
 {
 }
 
-impl<V: ClientValidationContext, E: ClientExecutionContext, T> ClientState<V, E> for T where
+impl<V: ClientValidationContext, E: ClientExecutionContext, T> ClientState<V, E> for T
+where
     T: Send + Sync + ClientStateCommon + ClientStateValidation<V> + ClientStateExecution<E>
 {
 }
